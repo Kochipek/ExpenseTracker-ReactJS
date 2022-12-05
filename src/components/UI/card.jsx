fd0c523f453a519a96a2d9acd29a588152fe97f5
ipek <ipekkochisarli@gmail.com>
2022-12-05 16:29:10 +0300

    new commit

--- src/components/UI/card.jsx
+++ src/components/UI/card.jsx
@@ -1,9 +1,7 @@
 import './card.css'
 const Card = (props) => {
     const classes = 'card ' + props.className;
-    //ustteki satiri eklemezsek sadece card css i calisir ama biz hepsinin birlikte calismasini istiyoruz
     return <div className={classes}>{props.children}</div>
-    //bu yuzden class name de {classes verdik yukarda}
     
 }
 
